3e86cd437d0421a97b96e0690bae2a497a099507
Muhammad Zaini <163093916+muhammadzaini213@users.noreply.github.com>
2026-06-30 18:54:19 +0800

    Feat/integration/gamescene (#32)
    * refactor: generators
    
    * refactor: map generator

---
+++ Assets/Scripts/Building/BuildingHealth.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using Game.Generic;
+using Game.Core.Effects;
+using Game.Manager;
+
+
+public class BuildingHealth : Health
+{
+    [SerializeField] private int ThreatPointValue;
+    [SerializeField] private int SubsPointValue;
+
+    private IVisualEffect[] effects;
+    protected override void Start()
+    {
+        base.Start();
+        effects = GetComponentsInChildren<IVisualEffect>();
+    }
+    public override void TakeDamage(float amount)
+    {
+        base.TakeDamage(amount);
+
+        foreach (IVisualEffect effect in effects)
+        {
+            effect.PlayEffect();
+        }
+    }
+
+    protected override void Die()
+    {
+        StreamChatManager.Instance.HandleStreamChat(StreamChatType.DESTROY_BUILDING, 3);
+        GameManager.Instance.AddThreat(ThreatPointValue);
+        GameManager.Instance.AddSubs(SubsPointValue);
+        base.Die();
+        Destroy(gameObject);
+    }
+}
